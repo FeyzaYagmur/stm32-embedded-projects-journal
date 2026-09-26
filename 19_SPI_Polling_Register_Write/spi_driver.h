79ada2228603046f85062b4e2240000b5a2b35c4
Feyza Yağmur Arat <feyzayagmurarat@gmail.com>
2026-09-26 18:32:43 +0300

    Add SPI driver header file with function prototype

---
+++ 19_SPI_Polling_Register_Write/spi_driver.h
@@ -0,0 +1,23 @@
+/**
+ * @file    spi_driver.h
+ * @brief   Synchronous SPI Peripheral Driver Header
+ * @author  Feyza Yagmur Arat
+ */
+
+#ifndef SPI_DRIVER_H
+#define SPI_DRIVER_H
+
+#include "main.h"
+
+/* Software Chip Select (CS) Pin Mapping */
+#define CS_GPIO_PORT            GPIOA
+#define CS_PIN                  GPIO_PIN_4
+
+/* Target Peripheral Register Map */
+#define REG_SHUTDOWN            0x0C  /* Power mode / shutdown register */
+#define REG_DATA_X              0x01  /* Target data / configuration register */
+
+/* Driver API function prototypes (Returns 1 on success, 0 on failure) */
+uint8_t SPI_Driver_Write_Reg(SPI_HandleTypeDef *hspi, uint8_t reg_addr, uint8_t data);
+
+#endif /* SPI_DRIVER_H */
